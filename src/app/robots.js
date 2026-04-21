ca153d3a7d78a9eb512536db016b6e924ce99e78
BaliBuddy <balibuddy@example.com>
2026-04-21 13:20:44 +0800

    Fix robots.js URL

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,5 +1,5 @@
 export default function robots() {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://balibuddy.com';
+  const baseUrl = 'https://balibuddy.vercel.app';
   
   return {
     rules: {
